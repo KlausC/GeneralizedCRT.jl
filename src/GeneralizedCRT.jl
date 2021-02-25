bccb67cd93ce08a19604666cbe846eb6739a8076
Klaus Crusius <klaus.crusius@web.de>
2021-02-25 16:57:08 +0100

    added algorithm and tests

--- src/GeneralizedCRT.jl
+++ src/GeneralizedCRT.jl
@@ -1,5 +1,45 @@
 module GeneralizedCRT
 
-# Write your package code here.
+export crt
+
+"""
+    crt(a, b, p, q)
+
+Given `0 <= a < p` and `0 <= b < q` with `a == b mod(gcd(p, q)`
+return unique solution `x ≡ a mod p and y ≡ b mod q` 
+with `0 <= x < lcm(p, q)`.
+
+Return `x, lcm(p, q)`.
+"""
+function crt(a::T, b::T, p::T, q::T) where T
+    c, u = gcdx(p, q)
+    q1 = div(q, c)
+    u = mod(u, q1)
+    iszero(mod(b - a, c)) || throw(ArgumentError("remainders must have same rest when diveded by ($a, $b, gcd($p, $q))"))
+    bac = div(b - a, c)
+    x = mod(bac * u, q1) * p + a
+    lcm = p * q1
+    return x, lcm
+end
+
+"""
+    crt(a::Vector, m::Vector)
+
+Given `0 <= a[i] < p[i] for all i` with `a[i] == a[j] mod(gcd(p[i], p[j]) for i != j`,
+return unique solution `x ≡ a[i] mod p[i] for all i`
+with `0 <= x < lcm(p, q)`.
+
+Return `x, lcm(p, q)`.
+
+"""
+function crt(a::AbstractVector{T}, m::AbstractVector{T}) where T
+    n = length(a)
+    n == length(m) || throw(ArgumentError("vectors of same size required"))
+    xI, lcmI = one(T), one(T)
+    for i = 1:n
+        xI, lcmI = crt(xI, a[i], lcmI, m[i])
+    end
+    xI, lcmI
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,38 @@
 using GeneralizedCRT
 using Test
 
-@testset "GeneralizedCRT.jl" begin
-    # Write your tests here.
+@testset "GeneralizedCRT" begin
+
+@testset "crt2" begin
+    a, b, p, q = 1, 11, 20, 50
+    @test crt(a, b, p, q) == (61, 100) 
+    a = 0
+    @test_throws ArgumentError crt(a, b, p, q)
+end
+
+@testset "crt" begin
+    a = [1, 11]
+    m = [20, 50]
+    @test crt(a, m) == crt(a..., m...)
+
+    a = [1, 1, 1, 1, 1, 0]
+    m = [2, 3, 4, 5, 6, 7]
+    @test crt(a, m) == (301, 420)
+
+    a = [5, 3, 1]
+    m = [6, 8, 10]
+    x, = crt(a, m)
+    @test a == mod.(x, m)
+
+    a = [2, 5, 7]
+    m = [6, 9, 15]
+    @test_throws ArgumentError crt(a, m)
+
+    a = vec([2  0  3  3  17  0  0  0  3  1])
+    m = vec([5  13  17  12  25  3  21  1  8  2])
+    x, lc = crt(a, m)
+    @test (x, lc) == (594867, 928200)
+    @test a == mod.(x, m)
+end
+
 end
