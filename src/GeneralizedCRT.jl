db5dd415cab456d5d9ba01702704b032ddc00cc6
KlausC <klausC@users.noreply.github.com>
2022-11-07 11:50:46 +0100

    enable for non-integers

--- src/GeneralizedCRT.jl
+++ src/GeneralizedCRT.jl
@@ -11,7 +11,7 @@ const THRESHOLD2 = 15 # split longer input arrays to different cpu threads
     crt(a, b, p, q)
 
 Given `0 <= a < p` and `0 <= b < q` with `a == b mod(gcd(p, q)`
-return unique solution `x ≡ a mod p` and `x ≡ b mod q` 
+return unique solution `x ≡ a mod p` and `x ≡ b mod q`
 with `0 <= x < lcm(p, q)`.
 
 Return `x, lcm(p, q)`.
@@ -20,36 +20,45 @@ function crt(a::A, b::B, p::C, q::D) where {A,B,C,D}
     crt(promote(a, b, p, q)...)
 end
 
-function crt(a::T, b::T, p::T, q::T) where T
+function crt(a::T, b::T, p::T, q::T) where {T}
     c, u = gcdx(p, q)
     q1 = div(q, c)
     u = mod(u, q1)
-    iszero(mod(b - a, c)) || throw(ArgumentError("remainders must have same rest when diveded by ($a, $b, gcd($p, $q))"))
+    if !iszero(mod(b - a, c))
+        t = "remainders ($a, $b) must have same rest when diveded by $(gcd(p, q)))"
+        throw(ArgumentError(t))
+    end
     bac = div(b - a, c)
     x = mod(bac * u, q1) * p + a
-    lcm = Base.checked_mul(p, q1)
+    lcm = T <: Integer ? Base.checked_mul(p, q1) : p * q1
     return x, lcm
 end
 
-function crt(ap::AbstractVector{<:Tuple{<:Integer,<:Integer}})
+function crt(ap::AbstractVector{<:Tuple{<:Any,<:Any}})
     crt([x[1] for x in ap], [x[2] for x in ap])
 end
-crt(t::Tuple{S,T}...) where {S<:Integer,T<:Integer} = crt(collect(t))
+crt(t::Tuple{S,T}...) where {S,T} = crt(collect(t))
 
 """
-    crt(a::Vector, m::Vector; nthreads, splitstreads, splitbinary)
+    crt(a::Vector, m::Vector; nt, splitstreads, splitbinary)
 
 Given `0 <= a[i] < p[i] for all i` with `a[i] == a[j] mod(gcd(p[i], p[j]) for i != j`,
 return unique solution `x ≡ a[i] mod p[i] for all i`
 with `0 <= x < lcm(p, q)`.
 
 Optional arguments:
-Use `nthreads=nthreads()` threads if `length(a) > splitthreads=($THRESHOLD2)`.
-Use binary algorithm is `length(a) > splitbinary=($THRESHOLD1)`.
+Use `nt=nthreads()` threads if `length(a) > splitthreads=($THRESHOLD2)`.
+Use binary algorithm if `length(a) > splitbinary=($THRESHOLD1)`.
 
 Return `x, lcm(p, q)`.
 """
-function crt(a::AbstractVector{T}, m::AbstractVector{S}; nt=nthreads()-1, splitthreads=THRESHOLD2, splitbinary=THRESHOLD1) where {S,T}
+function crt(
+    a::AbstractVector{T},
+    m::AbstractVector{S};
+    nt = nthreads() - 1,
+    splitthreads = THRESHOLD2,
+    splitbinary = THRESHOLD1,
+) where {S,T}
     # split input array and execute in parallel threads
     n = length(a)
     n == length(m) || throw(ArgumentError("vectors of same size required"))
@@ -69,7 +78,7 @@ function crt(a::AbstractVector{T}, m::AbstractVector{S}; nt=nthreads()-1, splitt
             nth -= 1
             av = view(a, n1:n2)
             mv = view(m, n1:n2)
-            tasks[i] = @spawn crts($av, $mv; splitbinary=$splitbinary)
+            tasks[i] = @spawn crts($av, $mv; splitbinary = $splitbinary)
         end
         a0 = Vector{R}(undef, nt)
         m0 = Vector{R}(undef, nt)
@@ -95,7 +104,7 @@ function crts(a::AbstractVector{T}, m::AbstractVector{S}; splitbinary) where {S,
         crtm(a, m)
     else
         n2 = (n + 1) ÷ 2
-        xI, lcmI = crts(view(a,1:n2), view(m, 1:n2); splitbinary)
+        xI, lcmI = crts(view(a, 1:n2), view(m, 1:n2); splitbinary)
         xJ, lcmJ = crts(view(a, n2+1:n), view(m, n2+1:n); splitbinary)
         crt(xI, xJ, lcmI, lcmJ)
     end
@@ -116,6 +125,6 @@ function crtm(a::AbstractVector{T}, m::AbstractVector{S}) where {S,T}
     xI, lcmI
 end
 
-_mod(a, b) = 0 <=a < b ? oftype(b, a) : mod(a, b)
+_mod(a, b) = 0 <= a < b ? oftype(b, a) : mod(a, b)
 
 end # module
